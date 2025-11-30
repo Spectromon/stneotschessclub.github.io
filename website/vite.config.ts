2674d5779bac9e7308392ce055da9434c55af353
Spectromon <tommycarter52@gmail.com>
2025-11-30 11:27:55 +0000

    update base again

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    base: '/stneotschessclub.github.io/',
+    base: 'stneotschessclub.github.io',
     build: {
       outDir: 'build',
     },
